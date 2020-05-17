c8795869c9c88808bd04e6c5bc7700f39a99c1fc
IsraelSistemas19 <israel_sistemas@hotmail.com>
2020-05-16 22:16:35 -0500

    se cambiaron comillas simples por dobles

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 	use \Psr\Http\Message\ResponseInterface as Response;
 
 	require_once "vendor/autoload.php";
-	require_once 'src/config/constants.php';
-	require_once 'src/config/config.php';
-	require_once 'src/config/conection.php';
-	require_once 'src/helpers/helpers.php';
-	require_once 'src/middlewares/middleware.php';
+	require_once "src/config/constants.php";
+	require_once "src/config/config.php";
+	require_once "src/config/conection.php";
+	require_once "src/helpers/helpers.php";
+	require_once "src/middlewares/middleware.php";
 
 	$config = ["settings" => [
-	    "addContentLengthHeader" => false,
-	    "displayErrorDetails" => true,
+	    "addContentLengthHeader" 			=> false,
+	    "displayErrorDetails" 				=> true,
 	    "determineRouteBeforeAppMiddleware" => true
 	]];
     
@@ -21,7 +21,7 @@
 	require_once "src/routes/routes.php";
 
 	$app->get("/", function (Request $request, Response $response, array $args) {
-	    $response->getBody()->write("API is running");
+	    $response->getBody()->write("API IS RUNNING");
 
 	    return $response;
 	});
